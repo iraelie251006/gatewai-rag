dfed7babf33112b4ee328b6ccf6076fe17339d07
Elie <niyubwayoiraelie5777@gmail.com>
2026-07-20 23:57:22 +0200

    Added sessionmaker configuration to database.py for session management

--- app/models/database.py
+++ app/models/database.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import os
 
 load_dotenv()
@@ -17,4 +18,10 @@ DATABASE_URL = (
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
+)
+
+SessionLocal = sessionmaker(
+    autoflush=False,
+    autocommit=False,
+    bind=engine,
 )
