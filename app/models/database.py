d44260c12d7ac81add223b17dec495a656156ce9
Elie <niyubwayoiraelie5777@gmail.com>
2026-07-20 23:54:23 +0200

    Updated environment variable names in database.py for consistency, fixing typos

--- app/models/database.py
+++ app/models/database.py
@@ -5,10 +5,10 @@ load_dotenv()
 
 DATABASE_URL = (
     f"postgresql+psycopg2://"
-    f"{os.getenv('DATABASE_USER')}:"
-    f"{os.getenv('DATABASE_PASSWORD')}@"
-    f"{os.getenv('DATABASE_HOST')}:"
-    f"{os.getenv('DATABASE_PORT')}/"
-    f"{os.getenv('DATABASE_NAME')}"
+    f"{os.getenv('DB_USER')}:"
+    f"{os.getenv('DB_PASSWORD')}@"
+    f"{os.getenv('DB_HOST')}:"
+    f"{os.getenv('DB_PORT')}/"
+    f"{os.getenv('DB_NAME')}"
     "?sslmode=require"
 )
